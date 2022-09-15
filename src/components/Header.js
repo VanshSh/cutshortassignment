5ae5ea688895081840851c47f58ea39440a39ecc
VanshSh <vanshsharma9354@gmail.com>
2022-09-15 21:31:39 +0530

    style: :lipstick:

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { TbStars } from 'react-icons/tb'
 
 const Header = () => {
     return (
-        <header className='mb-10 mr-16 bg-green-600 lg:bg-red-500   '>
+        <header className='mb-10  '>
             <h1 className='flex font-bold  text-3xl '>
                 <TbStars className='text-[#664de5] mx-2 ' />
                 Eden
