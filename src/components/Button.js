ba1b6353fb9d8e7ee87f12b9860aff95d9872dd1
VanshSh <vanshsharma9354@gmail.com>
2022-09-15 23:16:46 +0530

    style: :lipstick:

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,7 +7,7 @@ const Button = ({ title, path }) => {
         <button
             onClick={() => navigate(path)}
             className='bg-[#664de5] text-white lg:text-xl
-         px-20 rounded cursor-pointer my-8 py-3 text-md items-center justify-center flex'
+         lg:px-20 rounded cursor-pointer my-8 py-3 text-md items-center justify-center flex'
         >
             {title}
         </button>
--- src/pages/Planning.js
+++ src/pages/Planning.js
@@ -12,22 +12,22 @@ const Planning = () => {
             />
 
             <div className='flex flex-col lg:w-[70vh] '>
-                <div className='flex'>
-                    <div className='border border-[#664de5] cursor-pointer rounded lg:p-10 p-4 mr-10'>
+                <div className='flex lg:flex-row flex-col  '>
+                    <div className='border border-[#664de5] cursor-pointer rounded lg:p-10 p-4 lg:mr-10 mb-2'>
                         <BsFillPersonFill className='text-2xl text-[#664de5]' />
                         <h3 className='text-lg text-black font-bold mt-5'>
                             For myself
                         </h3>
-                        <p className='text-gray-700 font-semibold text-lg mt-4'>
+                        <p className='text-gray-700 font-semibold lg:text-lg text-md mt-4'>
                             Write better. Think more clearly. Stay organized.
                         </p>
                     </div>
-                    <div className='border  cursor-pointer rounded p-10 '>
+                    <div className='border cursor-pointer rounded lg:p-10 p-4 mb-2'>
                         <GrGroup className='text-2xl text-[#664de5]' />
                         <h3 className='text-lg text-black font-bold mt-5'>
                             With my team
                         </h3>
-                        <p className='text-gray-700 text-lg mt-4 font-semibold'>
+                        <p className='text-gray-700 font-semibold lg:text-lg text-md mt-4'>
                             Wikis, docs, tasks & projects, all in one place.
                         </p>
                     </div>
@@ -39,3 +39,13 @@ const Planning = () => {
 }
 
 export default Planning
+
+//   <div className='border  cursor-pointer rounded lg:p-10 p-4 '>
+//                     <GrGroup className='text-2xl text-[#664de5]' />
+//                     <h3 className='text-lg text-black font-bold mt-5'>
+//                         With my team
+//                     </h3>
+//                     <p className='text-gray-700 lg:text-lg text-sm mt-4 font-semibold'>
+//                         Wikis, docs, tasks & projects, all in one place.
+//                     </p>
+//                 </div>
