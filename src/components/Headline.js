7d299b2c4653e8ae08f6237fe98c8493e284f42c
VanshSh <vanshsharma9354@gmail.com>
2022-09-15 22:44:15 +0530

    style: :lipstick:

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -3,8 +3,10 @@ import React from 'react'
 const Headline = ({ mainHeading, subHeading }) => {
     return (
         <main className='text-center mb-10'>
-            <h1 className='lg:text-4xl font-bold '>{mainHeading}</h1>
-            <h4 className='mt-2 lg:text-xl text-gray-500'>{subHeading}</h4>
+            <h1 className='lg:text-4xl text-3xl font-bold '>{mainHeading}</h1>
+            <h4 className='mt-2 lg:text-xl text-lg text-gray-500'>
+                {subHeading}
+            </h4>
         </main>
     )
 }
